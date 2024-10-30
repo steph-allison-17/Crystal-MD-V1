2b6f998cf9db10917d56e617cab71ddb9c0536a4
Gmaxhacker1 <151476499+Gmaxhacker1@users.noreply.github.com>
2024-10-31 01:31:55 +0300

    Update main-ping.js

--- lazackcmds/main-ping.js
+++ lazackcmds/main-ping.js
@@ -7,8 +7,8 @@ let handler = async (m, { conn }) => {
     key: { fromMe: false, participant: `0@s.whatsapp.net`, remoteJid: 'status@broadcast' },
     message: {
       contactMessage: {
-        displayName: `Lazack md`,
-        vcard: `BEGIN:VCARD\nVERSION:3.0\nN:;a,;;;\nFN:'LAZACK MD'\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD`,
+        displayName: `Gcyber Bot`,
+        vcard: `BEGIN:VCARD\nVERSION:3.0\nN:;a,;;;\nFN:'𝐆𝐂𝐘𝐁𝐄𝐑 𝐁𝐎𝐓'\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD`,
       },
     },
   }
