79a94828680f26c0e8df91b558523467a2025f70
Gmaxhacker1 <151476499+Gmaxhacker1@users.noreply.github.com>
2024-11-03 13:45:06 +0300

    Update main-creator.js

--- gmaxbot/main-creator.js
+++ gmaxbot/main-creator.js
@@ -1,5 +1,5 @@
 let handler = async (m, { conn, usedPrefix, isOwner }) => {
-let vcard = `BEGIN:VCARD\nVERSION:3.0\nN:;lazack;;\nFN:lazack⁩\nORG:lazack\nTITLE:\nitem1.TEL;waid=255734980103:255734980103\nitem1.X-ABLabel:lazack\nX-WA-BIZ-DESCRIPTION:\nX-WA-BIZ-NAME:lazack\nEND:VCARD`
+let vcard = `BEGIN:VCARD\nVERSION:3.0\nN:;gmax;;\nFN:gmax\nORG:gmax\nTITLE:\nitem1.TEL;waid=255622053093:255622053093\nitem1.X-ABLabel:gmax\nX-WA-BIZ-DESCRIPTION:\nX-WA-BIZ-NAME:gmax\nEND:VCARD`
 await conn.sendMessage(m.chat, { contacts: { displayName: 'GMAX TECH', contacts: [{ vcard }] }}, {quoted: m})
 }
 handler.help = ['owner']
