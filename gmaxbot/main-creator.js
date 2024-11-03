66b7da6c0e7f3778f89e05d2fdf6fc7ca5a06b30
Lazack <164275612+Lazack28@users.noreply.github.com>
2024-11-03 13:05:13 +0300

    MTAJU

--- gmaxbot/main-creator.js
+++ gmaxbot/main-creator.js
@@ -1,6 +1,6 @@
 let handler = async (m, { conn, usedPrefix, isOwner }) => {
 let vcard = `BEGIN:VCARD\nVERSION:3.0\nN:;lazack;;\nFN:lazack⁩\nORG:lazack\nTITLE:\nitem1.TEL;waid=255734980103:255734980103\nitem1.X-ABLabel:lazack\nX-WA-BIZ-DESCRIPTION:\nX-WA-BIZ-NAME:lazack\nEND:VCARD`
-await conn.sendMessage(m.chat, { contacts: { displayName: 'LAZACK_28', contacts: [{ vcard }] }}, {quoted: m})
+await conn.sendMessage(m.chat, { contacts: { displayName: 'GMAX TECH', contacts: [{ vcard }] }}, {quoted: m})
 }
 handler.help = ['owner']
 handler.tags = ['main']
