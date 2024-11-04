b85f2363d3a37236ffcbfc647a3ef8afbcd7a3bb
Gmaxhacker1 <151476499+Gmaxhacker1@users.noreply.github.com>
2024-11-05 02:42:17 +0300

    Update main-runtime.js

--- gmaxbot/main-runtime.js
+++ gmaxbot/main-runtime.js
@@ -1,4 +1,4 @@
-//made with ❤️ by Shizo 
+//made with ❤️
 import { cpus as _cpus, totalmem, freemem } from 'os'
 import util from 'util'
 import os from 'os'
@@ -73,5 +73,5 @@ function clockString(ms) {
   let h = isNaN(ms) ? '--' : Math.floor(ms / 3600000) % 24
   let m = isNaN(ms) ? '--' : Math.floor(ms / 60000) % 60
   let s = isNaN(ms) ? '--' : Math.floor(ms / 1000) % 60
-  return [d, '\n *Days ☀️*\n ', h, ' *Hours 🕐*\n ', m, ' *Minute ⏰*\n ', s, ' *Second ⏱️* '].map(v => v.toString().padStart(2, 0)).join('')
+  return ['*｢💘 GCYBER MD UP TIME 💘」*\n\n ', d, ' *Days ☀️*\n ', h, ' *Hours 🕐*\n ', m, ' *Minute ⏰*\n ', s, ' *Second ⏱️* '].map(v => v.toString().padStart(2, 0)).join('')
 }
