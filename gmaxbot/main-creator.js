33d2f33bb173d4663a8bc356307aa94569a4f77e
Gmaxhacker1 <151476499+Gmaxhacker1@users.noreply.github.com>
2024-11-05 01:18:05 +0300

    Update main-creator.js

--- gmaxbot/main-creator.js
+++ gmaxbot/main-creator.js
@@ -1,9 +1,26 @@
 let handler = async (m, { conn, usedPrefix, isOwner }) => {
-let vcard = `BEGIN:VCARD\nVERSION:3.0\nN:;gmax;;\nFN:gmax\nORG:gmax\nTITLE:\nitem1.TEL;waid=255622053093:255622053093\nitem1.X-ABLabel:gmax\nX-WA-BIZ-DESCRIPTION:\nX-WA-BIZ-NAME:gmax\nEND:VCARD`
-await conn.sendMessage(m.chat, { contacts: { displayName: 'GMAX TECH', contacts: [{ vcard }] }}, {quoted: m})
-}
-handler.help = ['owner']
-handler.tags = ['main']
-handler.command = ['owner', 'creator', 'creador', 'dueño'] 
+  let vcard = `BEGIN:VCARD
+VERSION:2.0
+N:;Gmax;;;
+FN:Gmaxhacker
+ORG:GCYBER-Md
+TITLE:Owner
+item1.TEL;waid=255622053093:255622053093
+item1.X-ABLabel:Owner
+X-WA-BIZ-DESCRIPTION:Developer of the Bot
+X-WA-BIZ-NAME:Gmax
+END:VCARD`;
 
-export default handler
+  await conn.sendMessage(m.chat, {
+    contacts: {
+      displayName: 'Gmaxhacker,
+      contacts: [{ vcard }]
+    }
+  }, { quoted: m });
+}
+
+handler.help = ['owner'];
+handler.tags = ['main'];
+handler.command = ['creator', 'creador', 'dueño'];
+
+export default handler;
