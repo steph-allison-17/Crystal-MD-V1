1508c77d907ba767b7e5d1a0a044db514d7e7e0e
Gmaxhacker1 <151476499+Gmaxhacker1@users.noreply.github.com>
2024-11-05 02:55:34 +0300

    Update main-alive.js

--- gmaxbot/main-alive.js
+++ gmaxbot/main-alive.js
@@ -1,10 +1,7 @@
 let handler = async (m, { conn, text, usedPrefix, command }) => {
   // Sound
-  let name = m.pushName || conn.getName(m.sender)
-  var vn = 'https://cdn.jsdelivr.net/gh/Lazack28/Lazack-md@main/media/Alive.mp3'
-  let url = 'https://github.com/Lazack28/Lazack-md'
-  let murl = 'https://youtu.be/3j_EIP--2t8?si=4TFWV0On6Bl1wr-e'
-  let img = 'https://i.imgur.com/QMyKIPq.jpeg'
+  let name = m.pushName || conn.getName(m.sender);
+  let img = 'https://i.imgur.com/tBKTCku.jpeg';
   let con = {
     key: {
       fromMe: false,
@@ -17,34 +14,28 @@ let handler = async (m, { conn, text, usedPrefix, command }) => {
         vcard: `BEGIN:VCARD\nVERSION:3.0\nN:;a,;;;\nFN:${name}\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD`,
       },
     },
-  }
-  let doc = {
-    audio: {
-      url: vn,
-    },
-    mimetype: 'audio/mpeg',
-    ptt: true,
-    waveform: [100, 0, 100, 0, 100, 0, 100],
-    fileName: 'shizo',
+  };
 
+  let messageContent = {
+    text: 'GCYBER-MD 𝗜𝗦 𝗥𝗨𝗡𝗡𝗜𝗡𝗚', // Text content in case a message body is needed
     contextInfo: {
       mentionedJid: [m.sender],
       externalAdReply: {
-        title: 'LAZACK MD ALIVE',
-        body: 'Lazack md',
-        thumbnailUrl: img,
-        sourceUrl: 'https://chat.whatsapp.com/IIpL6gf6dcq4ial8gaJLE9',
+        title: '© GCYBER MD,
+        body: '© GCYBER MD,
+        thumbnailUrl: 'https://whatsapp.com/channel/0029VaFytPbAojYm7RIs6l1x',
         mediaType: 1,
         renderLargerThumbnail: true,
       },
     },
-  }
+  };
 
-  await conn.sendMessage(m.chat, doc, { quoted: con })
-}
+  // Send the message with the external ad reply
+  await conn.sendMessage(m.chat, messageContent, { quoted: con });
+};
 
-handler.help = ['alive']
-handler.tags = ['main']
-handler.command = /^(alive)$/i
+handler.help = ['alive'];
+handler.tags = ['main'];
+handler.command = /^(alive)$/i;
 
-export default handler
+export default handler;
